ae0228f0bc5153a5d89c07901212587239938cb3
ignaciolagunas <ign.lagunas@gmail.com>
2021-07-28 22:12:07 -0400

    Listo, creado el componente Task

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,7 @@
     <div>
       <AddTask @add-task="addTask"/>
       <ul>
-        <li class="task" v-for="(task, index) in tasks" :key="index">
-          {{index}}. {{task}}
-          <i @click="removeTask(index)" class="fas fa-times"></i>
-        </li>
+        <Task v-for="(task, index) in tasks" :key="index" :index="index" :taskText="task" @remove-task="removeTask" />
       </ul>
     </div>
   </div>
@@ -15,6 +12,7 @@
 
 <script>
 import AddTask from './components/AddTask.vue' 
+import Task from './components/Task.vue'
 
 export default {
   
@@ -36,6 +34,7 @@ export default {
 
   components:{
     AddTask,
+    Task
   },
 }
 </script>
@@ -51,24 +50,6 @@ export default {
   flex-direction: column;
   align-items: center;
 
-}
-
-.task {
-  background: #f4f4f4;
-  margin: 5px;
-  padding: 10px 20px;
-  list-style-type: none;
-  text-align: start;
-  display: flex;
-  justify-content: space-between;
-}
-
-i{
-   cursor: pointer;
-}
-
-i:hover{
-  color: red;
 }
 
 ul{
---
+++ src/components/Task.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <li class="task">{{index}}. {{taskText}}
+            <i class="fas fa-times" @click="removeTask(index)"></i>
+        </li>     
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Task",
+        props:{
+            taskText:{
+                type: String
+            },
+            index:{
+                type: Number
+
+            }
+        },
+        methods:{
+            removeTask(index){
+               this.$emit("remove-task", index)
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  list-style-type: none;
+  text-align: start;
+  display: flex;
+  justify-content: space-between;
+}
+
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  list-style-type: none;
+  text-align: start;
+  display: flex;
+  justify-content: space-between;
+}
+
+i{
+   cursor: pointer;
+}
+
+i:hover{
+  color: red;
+}
+
+
+
+</style>
